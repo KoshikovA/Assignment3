ada3d7e8f03987c3a88737934dc7a4f91bc4cd7d
YersinBerdikulov <221225@astanait.edu.kz>
2023-02-18 00:05:01 +0600

    package

--- Male/polyesterFeMale.java
+++
@@ -1,9 +0,0 @@
-package Male;
-
-public class   polyesterFeMale extends classicalOuterwearFemale {
-    public polyesterFeMale(String brand,String size, String color, double price, String gender, String season,int amount,String type,String material) {
-        super("Zara", "L", "black", 89000, "\n"+
-                "Female", "winter", 4, "\n"+
-                "Down jackets","classical","polyester");
-    }
-}
